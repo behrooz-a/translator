9b2ebfc81af4ba6b63ec9f821550f8919773bc92
Behrooz Abbaszadeh <90620602+behrooz-a@users.noreply.github.com>
2024-12-23 14:36:17 -0500

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,31 @@
+import gradio as gr
+import ollama
+
+def format_history(msg: str):
+    chat_history = [{"role": "system", "content": "You are an expert translator that translates English text to French and vice versa. Just translate and do not ask a question unless you did not understand the words or sentences."}]
+    chat_history.append({"role": "user", "content": msg})
+    return chat_history
+
+def translate(text):
+    chat_history = format_history(text)
+    response = ollama.chat(model='llama3.1', stream=True, messages=chat_history)
+    translated_text = ""
+    for partial_resp in response:
+        token = partial_resp["message"]["content"]
+        translated_text += token
+    return translated_text
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        gr.Image("EN.png", label="Icon")  
+    with gr.Row():
+        gr.Interface(
+            fn=translate,
+            inputs=gr.Textbox(label="Enter text to translate/ Saisissez le texte à traduire"),
+            outputs=gr.Textbox(label="Translated text/ Texte traduit")
+            #,title="English to French Translator"
+            #,description="Enter English text and get the French translation."
+        )
+
+# Launch the Gradio interface
+demo.launch(share=True)
